82c3b22d7488d315a87544fc10a4c59404e07bc1
Samuel0c <sleppiniemi@protonmail.com>
2017-11-03 22:26:49 +0200

    testi korjattu takaisin

--- src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -52,7 +52,7 @@ public class VarastoTest {
     
     @Test
     public void konstruktoriAntaaTVarastonTilavuudenOikeinNegatiivisellaTilavuudella() {
-        assertEquals(1, varastoNegTilavuus.getTilavuus(), vertailuTarkkuus);
+        assertEquals(0, varastoNegTilavuus.getTilavuus(), vertailuTarkkuus);
     }
     
     @Test
